fd15d9b0e7b2c266c3da2610ea71a61b62502074
binary-code-978 <93479009+binary-code-978@users.noreply.github.com>
2022-04-07 15:57:28 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-Wecam.set({
+Webcam.set({
     width: 350,
     height: 300,
     image_format: 'png',
@@ -10,7 +10,7 @@ camera = document.getElementById("camera");
 Webcam.attach('#camera');
 
 function take_snapshot() {
-    webcam.snap(function (data_uri) {
+    Webcam.snap(function (data_uri) {
         document.getElementById("result").innerHTML = '<img id="captured_image"src=' + data_uri + '"/>';
     });
 }
@@ -18,3 +18,22 @@ function take_snapshot() {
 console.log("ml5 version:", ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/PMfCnQoQj/', modelLoaded);
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3) * 100 + "%";
+    }
+}
+
+function modelLoaded() {
+    console.log("modelLoaded");
+}
